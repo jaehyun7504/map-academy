6b962ac6abe973e711978895fde399e102a38195
Jaehyeon Kim <jaehyun7504@gmail.com>
2021-03-16 16:41:32 +0900

    Simplified codes

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,31 +10,51 @@ dotenv.config({ path: path.join(__dirname, "..", "config.env") });
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.postSignup = async (req, res, next) => {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({
-      email: email,
-      password: hashedPassword,
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = new User({
+    email: email,
+    password: hashedPassword,
+  });
+  await user.save();
+  return res.status(200).json({
+    message: "success",
+    data: user,
+  });
+};
+
+exports.postLogin = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(404).json({
+      message: "error",
+      error: "입력하신 이메일로 가입한 이용자가 존재하지 않습니다.",
     });
-    await user.save();
+  }
+  const doMatch = await bcrypt.compare(password, user.password);
+  if (!doMatch) {
+    return res.status(404).json({
+      message: "error",
+      error: "비밀번호가 유효하지 않습니다.",
+    });
+  }
+  req.session.user = user;
+  req.session.isLoggedIn = true;
+  req.session.save((err) => {
     return res.status(200).json({
       message: "success",
       data: user,
     });
-  } catch (err) {
-    return res.status(400).json({
-      message: "error",
-      error: err.message,
-    });
-  }
+  });
 };
 
-exports.postLogin = async (req, res, next) => {
-  try {
+exports.postReset = async (req, res, next) => {
+  crypto.randomBytes(32, async (err, buf) => {
+    const token = buf.toString("hex");
     const email = req.body.email;
-    const password = req.body.password;
     const user = await User.findOne({ email: email });
     if (!user) {
       return res.status(404).json({
@@ -42,64 +62,18 @@ exports.postLogin = async (req, res, next) => {
         error: "입력하신 이메일로 가입한 이용자가 존재하지 않습니다.",
       });
     }
-    const doMatch = await bcrypt.compare(password, user.password);
-    if (!doMatch) {
-      return res.status(404).json({
-        message: "error",
-        error: "비밀번호가 유효하지 않습니다.",
-      });
-    }
-    req.session.user = user;
-    req.session.isLoggedIn = true;
-    req.session.save((err) => {
-      return res.status(200).json({
-        message: "success",
-        data: user,
-      });
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: "error",
-      error: err.message,
-    });
-  }
-};
-
-exports.postReset = async (req, res, next) => {
-  try {
-    crypto.randomBytes(32, async (err, buf) => {
-      try {
-        if (err) throw err;
-        const token = buf.toString("hex");
-        const email = req.body.email;
-        const user = await User.findOne({ email: email });
-        if (!user) {
-          return res.status(404).json({
-            message: "error",
-            error: "입력하신 이메일로 가입한 이용자가 존재하지 않습니다.",
-          });
-        }
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 300000;
-        await user.save();
-        const msg = {
-          to: req.body.email,
-          from: process.env.SENDGRID_SENDER_EMAIL,
-          subject: "[맵학원] 비밀번호 복구 요청",
-          html: `<p>비밀번호를 복구하려면 <a href="http://127.0.0.1:5000/reset/${token}">여기</a>를 클릭하세요.</p>`,
-        };
-        sgMail.send(msg);
-        return res.status(204).end();
-      } catch (err) {
-        throw new Error(err);
-      }
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: "error",
-      error: err.message,
-    });
-  }
+    user.resetToken = token;
+    user.resetTokenExpiration = Date.now() + 300000;
+    await user.save();
+    const msg = {
+      to: req.body.email,
+      from: process.env.SENDGRID_SENDER_EMAIL,
+      subject: "[맵학원] 비밀번호 복구 요청",
+      html: `<p>비밀번호를 복구하려면 <a href="http://127.0.0.1:5000/reset/${token}">여기</a>를 클릭하세요.</p>`,
+    };
+    sgMail.send(msg);
+    return res.status(204).end();
+  });
 };
 
 exports.getNewPassword = async (req, res, next) => {
