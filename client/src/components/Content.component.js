12f5dfe9c5d1e4fab5deb13a80bf23915a8d4563
Jaehyeon Kim <jaehyun7504@gmail.com>
2021-03-22 11:10:37 +0900

    Removed client dir

--- client/src/components/Content.component.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import useStyles from "../styles/Content.styles";
-
-function Content({ children }) {
-  const classes = useStyles();
-  return <section className={classes.Content}>{children}</section>;
-}
-
-export default Content;
